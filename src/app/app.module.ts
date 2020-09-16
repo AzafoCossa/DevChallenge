2c9e34f1f953a444b80d3244f02ab45aab8ee470
Azafo Cossa <azafoalexandre@outlook.com>
2020-09-16 10:08:32 +0200

    Componente que será responsável por exibir as informações dos países

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    CountriesComponent,
   ],
   imports: [
     BrowserModule,
