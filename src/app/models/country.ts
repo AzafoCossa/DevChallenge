12fb9d09bcd9f3e428ea3ce938821d128924eeae
Azafo Cossa <azafoalexandre@outlook.com>
2020-09-17 07:36:01 +0200

    Country Model

---
+++ src/app/models/country.ts
@@ -0,0 +1,11 @@
+export class Country {
+  nome: string;
+  capital: string;
+  regiao: string;
+  subRegiao: string;
+  populacao: number;
+  area: number;
+  fusoHorario: string;
+  nomeNativo: string;
+  link: string;
+}
