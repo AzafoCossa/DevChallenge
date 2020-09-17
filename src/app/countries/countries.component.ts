cafdc7dc9d50ff92e4bfec809490af5944d4d330
Azafo Cossa <azafoalexandre@outlook.com>
2020-09-17 16:16:15 +0200

    Integração de exceljs & file-saver

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Country } from '../models/country';
 import { CountryService } from '../services/country.service';
+import { Workbook } from 'exceljs';
+import * as fs from 'file-saver';
 
 @Component({
   selector: 'app-countries',
