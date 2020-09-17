378069bb75258156cc00f3bceda6c827b7021348
Azafo Cossa <azafoalexandre@outlook.com>
2020-09-17 07:40:54 +0200

    Componente responsável por buscar todos dados necessários sobre os países e apresentar na VIEW

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../models/country';
+import { CountryService } from '../services/country.service';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrls: ['./countries.component.scss']
+  styleUrls: ['./countries.component.scss'],
 })
 export class CountriesComponent implements OnInit {
-
-  constructor() { }
+  countries: Country[] = [];
+  constructor(private _countryService: CountryService) {}
 
   ngOnInit(): void {
+    this.getCountries();
   }
 
+  private getCountries() {
+    this._countryService.getCountries().subscribe(
+      (res) => {
+        this.countries = res;
+      },
+      (err) => console.error('Erro ao tentar carregar países')
+    );
+  }
 }
